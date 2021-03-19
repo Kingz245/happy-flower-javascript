5c3226c011571be567c6712c4895857e0ee991a7
Kingz245 <80926790+Kingz245@users.noreply.github.com>
2021-03-18 22:52:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let projectile: Sprite = null
+scene.setBackgroundColor(9)
+effects.bubbles.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . a a a a a a . . . . . 
+    . . . . a a 1 1 1 1 a a . . . . 
+    . . . . a 1 1 5 5 1 1 a . . . . 
+    . . . . a 1 1 5 5 1 1 a . . . . 
+    . . . . a a 1 1 1 1 a a . . . . 
+    . . . . . a a a a a a . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . 7 7 7 7 7 7 . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    `, SpriteKind.Player)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . e e . . e . . . . . 
+        . . . . . . . e e e e . . . . . 
+        . . . . . . . . . . e . . . . . 
+        . . . . . f 5 f 5 f 5 f 5 . . . 
+        . . . . . . 5 f 5 f 5 f 5 . . . 
+        . . . . . . . f 5 f 5 f 5 . . . 
+        . . . . . . . e . . e . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, randint(-25, 25), randint(-25, 25))
+    projectile.lifespan = 3000
+    if (projectile.vx < 0) {
+        projectile.image.flipX()
+    }
+})
